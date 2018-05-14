f5196c56212ddfa86161ab181f1a2e8b652ef464
ShengHui Wu <770577856@qq.com>
2018-05-14 21:13:25 +0800

    Exp 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Expirements for the Course of Digit Image Processing
+----
+## Exp 1
+#### Aims: Use region growing algorithm to split out the biggest two circles in the source image. 
+#### How to Run the Code:
+* Open the MATLAB, and change the work diractory to the root path of this code
+* Run this program without any parameters. Be sure that the source image `twoCircle.bmp` exits in `./src`
+* Wait till the figure appears on the screen, select two point on the image, one in each circle, then press enter and wait
+* The result will show on the screen and will be saved to `./result/twoCircle.bmp`
---
+++ areaGrow.m
@@ -0,0 +1,83 @@
+clear all
+%% load image and translate to gray
+global img
+img = imread('src/twoCircle.bmp');
+img = sum(img, 3) / 3;
+figure(1)
+subplot(1,2,1)
+imshow(img,[0, 255])
+%% select seed by click
+if( exist('x','var') == 0 && exist('y','var') == 0)  
+    [y,x] = getpts;%鼠标取点  回车确定  
+    x1 = round(x(1));
+    y1 = round(y(1));
+    x2 = round(x(2));  
+    y2 = round(y(2));
+end
+%% grow
+global resultImg
+global stack
+resultImg = zeros(size(img)); %img;
+
+stack = [[-1; -1], [-1; -1]];
+push(x1, y1);
+exist = nearestPoint(3);
+while(length(stack) ~= 2 || exist)
+        exist = nearestPoint(3);
+end
+
+stack = [[-1; -1], [-1; -1]];
+push(x2, y2);
+exist = nearestPoint(2);
+while(length(stack) ~= 2 || exist)
+        exist = nearestPoint(2);
+end
+
+subplot(1,2,2)
+imshow(resultImg,[0, 256])
+imwrite(resultImg, 'result/twoCircle.bmp');
+
+%% add point
+function exist = nearestPoint(threshold)
+    global img
+    global resultImg
+    global stack
+    exist = 0;
+
+    temp = stack(:, length(stack));
+    x = temp(1); y = temp(2);
+
+    current = img(x, y);
+    if(x ~= 1 && resultImg(x-1,y) ~= 256 && abs(img(x-1,y)-current) < threshold)% && img(x-1,y) > 130 && img(x-1,y) < 190)
+        push(x-1, y);
+        exist = 1;
+    end
+    if(x ~= 576 && resultImg(x+1,y) ~= 256 && abs(img(x+1,y)-current) < threshold)% && img(x+1,y) > 130 && img(x+1,y) < 190)
+       push(x+1,y);
+       exist = 1;
+    end
+    if(y ~= 1 && resultImg(x,y-1) ~= 256 && abs(img(x, y-1)-current) < threshold)% && img(x,y-1) > 130 && img(x,y-1) < 190)
+        push(x, y-1);
+        exist = 1;
+    end
+    if (y ~= 593 && resultImg(x,y+1) ~= 256 && abs(img(x, y+1)-current) < threshold)% && img(x,y+1) > 130 && img(x,y+1) < 190)
+        push(x, y+1);
+        exist = 1;
+    end
+    if (~exist)
+       pop(); 
+    end
+end
+
+%% stack operation 
+function pop()
+    global stack
+    stack = stack(:, 1:length(stack)-1); 
+end
+
+function push(x, y)
+    global stack
+    global resultImg
+    stack = [stack, [x; y]];
+    resultImg(x, y) = 256;
+end
